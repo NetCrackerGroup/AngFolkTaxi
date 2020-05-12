0ea5b7d8d4df87681757c0d18b8af64cb1ff0a27
Aleksey Palienko <aleksey-palienko@mail.ru>
2020-05-13 01:04:57 +0300

    index pagr 1.0

--- src/app/city-map/city-map.component.ts
+++ src/app/city-map/city-map.component.ts
@@ -4,6 +4,8 @@ import {IRoute} from '../entities/iroute';
 import {YandexMapComponent} from 'angular8-yandex-maps/lib/components/yandex-map-component/yandex-map.component';
 import {any} from 'codelyzer/util/function';
 import {AngularYandexMapsModule} from 'angular8-yandex-maps';
+import {AuthService} from '../services/auth.service';
+import {LoginComponent} from '../login/login.component';
 
 @Component({
   selector: 'app-city-map',
@@ -13,8 +15,15 @@ import {AngularYandexMapsModule} from 'angular8-yandex-maps';
 export class CityMapComponent implements OnInit {
 
   listRoutes: IRoute[] = null;
+  authService: AuthService;
+  loginComponent: LoginComponent;
 
-  constructor(private  routeService: RoutesService) { }
+  constructor(private  routeService: RoutesService,
+              authService: AuthService,
+              loginComponent: LoginComponent) {
+      this.authService = authService;
+      this.loginComponent = loginComponent;
+  }
 
   ngOnInit() {
     this.routeService.getRandomRoutes().subscribe(
@@ -30,4 +39,7 @@ export class CityMapComponent implements OnInit {
 
   }
 
+  isReg(): boolean {
+    return this.authService.logIn;
+  }
 }
