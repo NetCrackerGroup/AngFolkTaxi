a4be08e494c46070faab7f21dd5d5c18c960f111
Sigarev Aleksei <afakusima@mail.ru>
2020-05-22 08:35:10 +0300

    fixs group link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   devUrl : 'http://142.93.130.144:1337',
-  devUrlFront : 'http://142.93.130.144:4200'
+  devUrlFront : 'http://142.93.130.144:8080'
 };
